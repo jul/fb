c44c2b2bbd0a2d56c6289a508b4c52d1849a57a5
julien tayon <julien@tayon.net>
2021-01-18 17:17:53 +0100

    amélioration de hello world une sinusoidale sur l'écran le caractère joker quand non implémenté dans la fonte (le dessin et moi...) un bel effet de gradient du nettoyage à faire

--- hello.pl
+++ hello.pl
@@ -13,6 +13,16 @@ my %font=();
     
 $font{"void"} =[ 
     0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 1, 0, 1, 0, 1, 0, 
+    0, 0, 1, 1, 0, 0, 1, 0,
+    0, 0, 0, 1, 0, 0, 1, 0,
+    0, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 
+   ];
+$font{" "} =[ 
+    0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 
     0, 0, 0, 0, 0, 0, 0, 0,
@@ -84,20 +94,34 @@ sub next_line{
    sysseek $fh, $stride - ( $so_pixel_array * $so_pixel), SEEK_CUR;
 
 };
+my @cmap =  map { ((($_)+220) %256 , ($_ * 8) % 256, (255 - ($_))%256, 255 )  } (0 .. 32);
+sub get_col { my ($index) = @_;$index=$index%32 ; return @cmap[($index*4)..(($index+1)*4-1)]; }
+
+
+
+my $CI=0;
 sub pr_char{ 
-    my ($fh, $x, $y, $letter)=@_;
-    go_at $fh, $x, $y;
+    my ($fh, $x, $y, $letter,$ci)=@_;
     my @black = (0, 0, 0, 255);
     my @white = (255, 255,255,255);
+
+
+    go_at $fh, $x, $y;
     my $c=0;
     my $line="";
+    my @color= @black;
     my @char = exists($font{$letter}) ? @{$font{$letter}} : @{$font{"void"}};
+    if ($letter !~ / /) {
+        @color = get_col ((($x+$y)/32)%32);
+        #print $letter . "," . $x . ",". $x%32 . "\n";
+        $CI=$CI%32;
+   }
     for my $pixel ( @char) {
 
-        $line.= pack "C4", $pixel ? @white: @black;
-        $line.= pack "C4", $pixel ? @white: @black;
-        $line.= pack "C4", $pixel ? @white: @black;
-        $line.= pack "C4", $pixel ? @white: @black;
+        $line.= pack "C4", $pixel ? @color: @black;
+        $line.= pack "C4", $pixel ? @color: @black;
+        $line.= pack "C4", $pixel ? @color: @black;
+        $line.= pack "C4", $pixel ? @color: @black;
         if (($c++%8)==7) {
             syswrite $fh, $line;
             next_line $fh, $font{"width"} * 4;
@@ -123,17 +147,18 @@ sub nl {
     go_at $fb, $COL*$CW, $LIN*$LW;
 }
 sub print_ {
+
     for my $c (split //,$_[0]) {
         if ($c =~ /"\n"/ ) {
             nl;
         } else {
-            pr_char $fb, $COL++ * $CW, $LIN * $LW, $c;
+            pr_char $fb, $COL++ * $CW, $LIN * $LW, $c, $CI;
+            $CI=$CI%32;
         }
     }
     nl;
 }
-for my $i (0 ... 15) { 
-
-    print_ " " x (int(cos($i/3)*10) + 10 )  .  "hello lol world";
+for my $i (0 ... 30) { 
+    print_ " " x (int(cos($i/3)*15) + 20 )  .  "hello lol world";
 }
 close($fb)
